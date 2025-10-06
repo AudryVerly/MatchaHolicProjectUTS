93b86d47e86990963ff3dbe55d94de7da72b1036
Audry Verly <audry.verly@gmail.com>
2025-10-06 16:03:49 +0700

    Update Detail

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -21,35 +21,98 @@ class Detail extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Detail Profil')),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CircleAvatar(radius: 60, backgroundImage: NetworkImage(photo)),
-            Divider(height: 10, color: Colors.transparent),
-            const SizedBox(height: 15),
-            Text(
-              name,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        child: Center(
+          child: Card(
+            color: Colors.grey[200],
+            margin: const EdgeInsets.all(20),
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
             ),
-            Text('NRP: $nrp'),
-            Divider(height: 10, color: Colors.transparent),
-            Card(
-              color: Colors.grey,
-              margin: EdgeInsets.all(15),
-              child: ListTile(
-                title: const Text('Program/ Lab'),
-                subtitle: Text(program),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: NetworkImage(photo),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('NRP: $nrp'),
+                  const SizedBox(height: 20),
+
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.4),
+                          blurRadius: 5,
+                          offset: const Offset(2, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Program / Lab',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(program),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 15),
+
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.4),
+                          blurRadius: 5,
+                          offset: const Offset(2, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Biografi',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(biografi)
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            Divider(height: 10, color: Colors.transparent),
-            Card(
-              color: Colors.grey,
-              margin: EdgeInsets.all(15),
-              child: ListTile(
-                title: const Text('Biografi'),
-                subtitle: Text(biografi),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
