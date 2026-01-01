2e1b2138bd5d4d51775c9f02209c6131d3be1bb5
Mathew Reynald <mathew.reynald04@gmail.com>
2026-01-01 19:24:45 +0700

    penyempurnaan tampilan login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -96,7 +96,6 @@ class _LoginState extends State<Login> {
       appBar: AppBar(title: Text('Login')),
 
       body: Container(
-        height: 300,
         margin: EdgeInsets.all(20),
         padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
@@ -105,139 +104,141 @@ class _LoginState extends State<Login> {
           color: Colors.white,
           boxShadow: [BoxShadow(blurRadius: 5)],
         ),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  hintText: 'Enter valid email id as abc@gmail.com',
-                  errorText: _emailError.isEmpty ? null : _emailError,
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _user_email = value;
-                    _emailError = _validateEmail(value)
-                        ? ''
-                        : 'Invalid email format';
-                  });
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: _pwdController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter secure password',
-                ),
-                onChanged: (value) {
-                  _user_password = value;
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Container(
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: ElevatedButton(
-                  onPressed: () {
-                    doLogin();
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com',
+                    errorText: _emailError.isEmpty ? null : _emailError,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      _user_email = value;
+                      _emailError = _validateEmail(value)
+                          ? ''
+                          : 'Invalid email format';
+                    });
                   },
-                  child: Text('Login', style: TextStyle(fontSize: 25)),
                 ),
               ),
-            ),
-            if (_error_login.isNotEmpty)
-              Text(_error_login, style: TextStyle(color: Colors.red)),
-
-            // Register Link
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Belum memiliki akun? '),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Register()),
-                      );
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: _pwdController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password',
+                  ),
+                  onChanged: (value) {
+                    _user_password = value;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      doLogin();
                     },
-                    child: const Text(
-                      'Daftar di sini',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
+                    child: Text('Login', style: TextStyle(fontSize: 25)),
+                  ),
+                ),
+              ),
+              if (_error_login.isNotEmpty)
+                Text(_error_login, style: TextStyle(color: Colors.red)),
+
+              // Register Link
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Belum memiliki akun? '),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Register()),
+                        );
+                      },
+                      child: const Text(
+                        'Daftar di sini',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            // Padding(
-            //   padding: EdgeInsets.all(10),
-            //   child: Container(
-            //     height: 50,
-            //     width: 300,
-            //     decoration: BoxDecoration(
-            //       borderRadius: BorderRadius.circular(20),
-            //     ),
-            //     child: ElevatedButton(
-            //       onPressed: () async {
-            //         if (_validateEmail(active_user)) {
-            //           // Call the login function
-            //           bool successLogin = await doLogin(
-            //             active_user,
-            //             _pwdController.text,
-            //           );
-            //           if (!successLogin) {
-            //             //if_the_login_failed
-            //             showDialog(
-            //               context: context,
-            //               builder: (BuildContext context) {
-            //                 return AlertDialog(
-            //                   title: const Text("Login Failed"),
-            //                   content: const Text(
-            //                     "Incorrect email or password. Please try again.",
-            //                   ),
-            //                   shape: RoundedRectangleBorder(
-            //                     borderRadius: BorderRadius.circular(12),
-            //                   ),
-            //                   actions: [
-            //                     TextButton(
-            //                       child: const Text("OK"),
-            //                       onPressed: () {
-            //                         Navigator.popAndPushNamed(context, 'login');
-            //                       },
-            //                     ),
-            //                   ],
-            //                 );
-            //               },
-            //             );
-            //           }
-            //         } else {
-            //           setState(() {
-            //             _emailError = 'Invalid email format';
-            //           });
-            //         }
-            //       },
-            //       child: Text('Login', style: TextStyle(fontSize: 25)),
-            //     ),
-            //   ),
-            // ),
-          ],
+              // Padding(
+              //   padding: EdgeInsets.all(10),
+              //   child: Container(
+              //     height: 50,
+              //     width: 300,
+              //     decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.circular(20),
+              //     ),
+              //     child: ElevatedButton(
+              //       onPressed: () async {
+              //         if (_validateEmail(active_user)) {
+              //           // Call the login function
+              //           bool successLogin = await doLogin(
+              //             active_user,
+              //             _pwdController.text,
+              //           );
+              //           if (!successLogin) {
+              //             //if_the_login_failed
+              //             showDialog(
+              //               context: context,
+              //               builder: (BuildContext context) {
+              //                 return AlertDialog(
+              //                   title: const Text("Login Failed"),
+              //                   content: const Text(
+              //                     "Incorrect email or password. Please try again.",
+              //                   ),
+              //                   shape: RoundedRectangleBorder(
+              //                     borderRadius: BorderRadius.circular(12),
+              //                   ),
+              //                   actions: [
+              //                     TextButton(
+              //                       child: const Text("OK"),
+              //                       onPressed: () {
+              //                         Navigator.popAndPushNamed(context, 'login');
+              //                       },
+              //                     ),
+              //                   ],
+              //                 );
+              //               },
+              //             );
+              //           }
+              //         } else {
+              //           setState(() {
+              //             _emailError = 'Invalid email format';
+              //           });
+              //         }
+              //       },
+              //       child: Text('Login', style: TextStyle(fontSize: 25)),
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
